84c6c55d4a861aceaf608bb806bdc5d3747fcf39
Andrew Baggaley <a.w.baggaley@gmail.com>
2013-06-23 21:21:21 +0100

    more tweaks

--- bin/vortex/vortex_density.m
+++ bin/vortex/vortex_density.m
@@ -2,6 +2,7 @@ function N=vortex_density(filenumber,varargin)
 global dims box_size
 global x y z
 global f u u2 v_curv u_mf
+global u_mf_x u_mf_y u_mf_z
 global number_of_particles
 global Density
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -18,10 +19,11 @@ N=16;
 xbox=-box_size(1)/2:box_size(1)/N:box_size(1)/2;
 ybox=-box_size(2)/2:box_size(2)/N:box_size(2)/2;
 zbox=-box_size(3)/2:box_size(3)/N:box_size(3)/2;
-Density(N,N,N)=0.;
-Curv(N,N,N)=0.;
-Count(N,N,N)=0.;
-Friction(N,N,N)=0.;
+Density(1:N,1:N,1:N)=0.;
+Curv(1:N,1:N,1:N)=0.;
+Count(1:N,1:N,1:N)=0.;
+Friction(1:N,1:N,1:N)=0.;
+allFriction(1:N,1:N,1:N,1:3)=0.;
 for j=1:number_of_particles
     if round(f(j))==0
     else
@@ -43,6 +45,9 @@ for j=1:number_of_particles
                        Count(ii,jj,kk)=Count(ii,jj,kk)+1;
                        Curv(ii,jj,kk)=Curv(ii,jj,kk)+v_curv(j);
                        Friction(ii,jj,kk)=Friction(ii,jj,kk)+u_mf(j);
+                       allFriction(ii,jj,kk,1)=allFriction(ii,jj,kk,1)+u_mf_x(j);
+                       allFriction(ii,jj,kk,2)=allFriction(ii,jj,kk,2)+u_mf_y(j);
+                       allFriction(ii,jj,kk,3)=allFriction(ii,jj,kk,3)+u_mf_z(j);
                     end
                 end
             end
@@ -50,57 +55,112 @@ for j=1:number_of_particles
       end
     end
 end
+%------------------------
 Curv=Curv./Count;
 tf=isnan(Curv);
 Curv(tf)=0.;
+%------------------------
 Friction=Friction./Count;
 tf=isnan(Friction);
 Friction(tf)=0.;
+%------------------------
+allFriction(:,:,:,1)=allFriction(:,:,:,1)./Count;
+allFriction(:,:,:,2)=allFriction(:,:,:,2)./Count;
+allFriction(:,:,:,3)=allFriction(:,:,:,3)./Count;
+tf=isnan(allFriction);
+allFriction(tf)=0.;
+%------------------------
 if strcmp(p.Results.Plots,'density')
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'xz')
-    pcolor(xbox(1:N),zbox(1:N),squeeze(sum(Density,2))) ; shading interp
+    pcolor(xbox(1:N),zbox(1:N),squeeze(sum(Density,2))') ; shading interp
     daspect([1 1 1])
   end
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'xy')
     figure
-    pcolor(xbox(1:N),ybox(1:N),squeeze(sum(Density(:,:,:),3))) ; shading interp
+    pcolor(xbox(1:N),ybox(1:N),squeeze(sum(Density(:,:,:),3))') ; shading interp
     daspect([1 1 1])
   end
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'yz')
     figure
-    pcolor(ybox(1:N),zbox(1:N),squeeze(sum(Density(:,:,:),1))) ; shading interp
+    pcolor(ybox(1:N),zbox(1:N),squeeze(sum(Density(:,:,:),1))') ; shading interp
     daspect([1 1 1])
   end
 elseif  strcmp(p.Results.Plots,'curv')
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'xz')
   figure
-  pcolor(xbox(1:N),zbox(1:N),squeeze(sum(Curv,2))) ; shading interp
+  pcolor(xbox(1:N),zbox(1:N),squeeze(sum(Curv,2))') ; shading interp
   daspect([1 1 1])
   end
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'xy')
   figure
-  pcolor(xbox(1:N),ybox(1:N),squeeze(sum(Curv(:,:,:),3))) ; shading interp
+  pcolor(xbox(1:N),ybox(1:N),squeeze(sum(Curv(:,:,:),3))') ; shading interp
   daspect([1 1 1])
   end
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'yz')
   figure
-  pcolor(ybox(1:N),zbox(1:N),squeeze(sum(Curv(:,:,:),1))) ; shading interp
+  pcolor(ybox(1:N),zbox(1:N),squeeze(sum(Curv(:,:,:),1))') ; shading interp
   daspect([1 1 1])
   end
 elseif  strcmp(p.Results.Plots,'friction')
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'xz')
   figure
-  pcolor(xbox(1:N),zbox(1:N),squeeze(sum(Friction,2))) ; shading interp
+  pcolor(xbox(1:N),zbox(1:N),squeeze(sum(Friction,2))') ; shading interp
   daspect([1 1 1])
   end
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'xy')
   figure
-  pcolor(xbox(1:N),ybox(1:N),squeeze(sum(Friction(:,:,:),3))) ; shading interp
+  pcolor(xbox(1:N),ybox(1:N),squeeze(sum(Friction(:,:,:),3))') ; shading interp
   daspect([1 1 1])
   end
   if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'yz')
   figure
-  pcolor(ybox(1:N),zbox(1:N),squeeze(sum(Friction(:,:,:),1))) ; shading interp
+  pcolor(ybox(1:N),zbox(1:N),squeeze(sum(Friction(:,:,:),1))') ; shading interp
   daspect([1 1 1])
+  end
+elseif  strcmp(p.Results.Plots,'allfriction')
+  if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'xz')
+  figure
+  subplot(3,1,1)
+  pcolor(xbox(1:N),zbox(1:N),squeeze(sum(allFriction(:,:,:,1),2))') ; shading interp
+  daspect([1 1 1])
+  colorbar
+  subplot(3,1,2)
+  pcolor(xbox(1:N),zbox(1:N),squeeze(sum(allFriction(:,:,:,2),2))') ; shading interp
+  daspect([1 1 1])
+  colorbar
+  subplot(3,1,3)
+  pcolor(xbox(1:N),zbox(1:N),squeeze(sum(allFriction(:,:,:,3),2))') ; shading interp
+  daspect([1 1 1])
+  colorbar
+  end
+  if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'xy')
+  figure
+  subplot(3,1,1)
+  pcolor(xbox(1:N),ybox(1:N),squeeze(sum(allFriction(:,:,:,1),3))') ; shading interp
+  daspect([1 1 1])
+  colorbar
+  subplot(3,1,2)
+  pcolor(xbox(1:N),ybox(1:N),squeeze(sum(allFriction(:,:,:,2),3))') ; shading interp
+  daspect([1 1 1])
+  colorbar
+  subplot(3,1,3)
+  pcolor(xbox(1:N),ybox(1:N),squeeze(sum(allFriction(:,:,:,3),3))') ; shading interp
+  daspect([1 1 1])
+  colorbar
+  end
+  if strcmp(p.Results.PlotPlane,'all') || strcmp(p.Results.PlotPlane,'yz')
+  figure
+  subplot(1,3,1)
+  pcolor(ybox(1:N),zbox(1:N),squeeze(sum(allFriction(:,:,:,1),1))') ; shading interp
+  daspect([1 1 1])
+  colorbar
+  subplot(1,3,2)
+  pcolor(ybox(1:N),zbox(1:N),squeeze(sum(allFriction(:,:,:,2),1))') ; shading interp
+  daspect([1 1 1])
+  colorbar
+  subplot(1,3,3)
+  pcolor(ybox(1:N),zbox(1:N),squeeze(sum(allFriction(:,:,:,3),1))') ; shading interp
+  daspect([1 1 1])
+  colorbar
   end
 end
