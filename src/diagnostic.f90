d3bcff9d22cdaedb08cbdc1713d814a9a8414a72
Andrew Baggaley <abaggaley@euclid-01.maths.gla.ac.uk>
2014-05-11 18:48:42 +0100

    added friction force need to merge

--- src/diagnostic.f90
+++ src/diagnostic.f90
@@ -2,10 +2,12 @@
 module diagnostic
   use cdata
   use general
+  use normal_fluid
   contains
   !>dummy routine to call all diagnostic routines
   subroutine calculate_diagnostics()
     implicit none
+    call friction_force
     if (mod(itime, shots)==0) then
       call velocity_info !diagnostics.mod
       call curv_info !diagnostics.mod
@@ -47,5 +49,42 @@ module diagnostic
     kappa_max=maxval(curvi)
     kappa_min=minval(curvi,mask=curvi>0)
     deallocate(curvi) !deallocate helper array
+  end subroutine
+  subroutine friction_force
+    integer :: i
+    real :: f_dot(3)  
+    real :: u_norm(3)
+    real :: disti
+    real :: force(3), force2(3), force3(3), force4(3)
+    real :: max_force(pcount)
+    real, parameter :: D=2.79E-5
+    real, parameter :: Dt=-4.14E-6
+    force=0. ; force2=0. ; force3=0. ; force4=0.
+    open(unit=76,file='./test_friction.log')
+     do i=1, pcount
+      if (f(i)%infront==0) cycle
+
+        call get_deriv_1(i,f_dot)
+        call get_normal_velocity(f(i)%x,u_norm)
+       disti=dist_gen(f(i)%x,f(i)%ghosti) !general.f90
+       force=force+cross_product(f_dot,(u_norm-(f(i)%u-f(i)%u_mf)))*disti
+       force2=force2+cross_product(f_dot,cross_product(f_dot,(u_norm-(f(i)%u-f(i)%u_mf))))*disti
+       force3=force3+abs(D*cross_product(f_dot,(u_norm-(f(i)%u-f(i)%u_mf)))*disti+&
+              Dt*cross_product(f_dot,cross_product(f_dot,(u_norm-(f(i)%u-f(i)%u_mf))))*disti)
+       force4=force4+abs(D*cross_product(f_dot,(u_norm-(f(i)%u-f(i)%u_mf)))*disti)+&
+              abs(Dt*cross_product(f_dot,cross_product(f_dot,(u_norm-(f(i)%u-f(i)%u_mf))))*disti)
+       max_force(i)=sqrt(dot_product(force3,force3))
+    end do
+   print*, force
+   print*, force2
+   force=force*D
+   force2=force2*Dt
+   print*, sqrt(dot_product(force+force2,force+force2))/(0.1*0.1*0.2)
+   !print*, sqrt(dot_product(abs(force)+abs(force2),abs(force)+abs(force2)))/(0.1*0.1*0.2)
+   print*, sqrt(dot_product(force3,force3))/(0.1*0.1*0.2)
+   print*, maxval(force3)
+   !print*, sqrt(dot_product(force4,force4))/(0.1*0.1*0.2)
+   close(76)
+   stop
   end subroutine
 end module
